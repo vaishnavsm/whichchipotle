c61a0017402c0199554f0c2ae63fbe3a26269e00
Vaishnav <vaishnavsm@gmail.com>
2022-05-17 08:21:47 -0500

    Change colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,26 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      yellow: colors.yellow,
+      brown: {
+        500: '#A57F60'
+      },
+      fg: {
+        400: '#E8AE68',
+        200: '#FFD275',
+        'A': '#DB5A42'
+      }
+    }
   },
   plugins: [],
 }
